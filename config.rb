346ebc3797b505cdc1af170ac688b0010fd74e28
Paul Kwiatkowski <pnkwiatkowski@gmail.com>
2016-11-28 02:34:05 -0600

    Snapshot version of site published.

---
+++ config.rb
@@ -0,0 +1,43 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+activate :directory_indexes
+
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+end
